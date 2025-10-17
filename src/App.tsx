4ee726b7ac4ae522e737957e7460eaa2e421506d
Willie <williehemail@gmail.com>
2025-10-16 17:47:19 -0700

    add translator and lagnauge detect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,162 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // 👇 Add explicit types to each ref
+    const inputRef = useRef<HTMLTextAreaElement>(null);
+    const outputRef = useRef<HTMLOutputElement>(null);
+    const detectedRef = useRef<HTMLSpanElement>(null);
+    const formRef = useRef<HTMLFormElement>(null);
+    const languageRef = useRef<HTMLSelectElement>(null);
+    const notSupportedRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const run = async () => {
+            if (!("LanguageDetector" in self)) {
+                if (notSupportedRef.current)
+                    notSupportedRef.current.hidden = false;
+                return;
+            }
+
+            if (!formRef.current) return;
+            formRef.current.style.visibility = "visible";
+            const detector = await (self as any).LanguageDetector.create();
+
+            const languageTagToHumanReadable = (
+                languageTag: string,
+                targetLanguage: string
+            ) => {
+                const displayNames = new Intl.DisplayNames([targetLanguage], {
+                    type: "language",
+                });
+                return displayNames.of(languageTag);
+            };
+
+            if (!inputRef.current || !detectedRef.current) return;
+
+            inputRef.current.addEventListener("input", async () => {
+                const value = inputRef.current?.value.trim();
+                if (!value) {
+                    detectedRef.current!.textContent =
+                        "not sure what language this is";
+                    return;
+                }
+
+                const [{ detectedLanguage, confidence }] =
+                    await detector.detect(value);
+                detectedRef.current!.textContent = `${(
+                    confidence * 100
+                ).toFixed(1)}% sure that this is ${languageTagToHumanReadable(
+                    detectedLanguage,
+                    "en"
+                )}`;
+            });
+
+            inputRef.current.dispatchEvent(new Event("input"));
+
+            if ("Translator" in self) {
+                formRef.current.addEventListener("submit", async (e) => {
+                    e.preventDefault();
+                    try {
+                        const sourceLanguage = (
+                            await detector.detect(
+                                inputRef.current!.value.trim()
+                            )
+                        )[0].detectedLanguage;
+                        const targetLanguage = languageRef.current!.value;
+
+                        const availability = await (
+                            self as any
+                        ).Translator.availability({
+                            sourceLanguage,
+                            targetLanguage,
+                        });
+
+                        if (availability === "unavailable") {
+                            outputRef.current!.textContent = `${languageTagToHumanReadable(
+                                sourceLanguage,
+                                "en"
+                            )} - ${languageTagToHumanReadable(
+                                targetLanguage,
+                                "en"
+                            )} pair is not supported.`;
+                            return;
+                        }
+
+                        const translator = await (
+                            self as any
+                        ).Translator.create({
+                            sourceLanguage,
+                            targetLanguage,
+                            monitor(m: any) {
+                                m.addEventListener(
+                                    // copied from example, might need to change ltr
+                                    "downloadprogress",
+                                    (e: any) => {
+                                        console.log(
+                                            `Downloaded ${e.loaded * 100}%`
+                                        );
+                                    }
+                                );
+                            },
+                        });
+                        outputRef.current!.textContent =
+                            await translator.translate(
+                                inputRef.current!.value.trim()
+                            );
+                    } catch (err: any) {
+                        outputRef.current!.textContent =
+                            "An error occurred. Please try again.";
+                        console.error(err.name, err.message);
+                    }
+                });
+            }
+        };
+
+        run();
+    }, []);
+
+    return (
+        <>
+            <h1>💬 Translator and Language Detector API Playground</h1>
+            <div ref={notSupportedRef} className="not-supported-message" hidden>
+                Your browser doesn't support the Translator or Language Detector
+                APIs.
+            </div>
+            <form ref={formRef}>
+                <label htmlFor="input">Input:</label>
+                <textarea
+                    id="input"
+                    ref={inputRef}
+                    defaultValue={"Hello, world!"}
+                ></textarea>
+                <p>
+                    I'm{" "}
+                    <span ref={detectedRef}>
+                        not sure what language this is
+                    </span>
+                    .
+                </p>
+
+                <label htmlFor="translate">
+                    Translate to
+                    <select id="translate" ref={languageRef} defaultValue="es">
+                        <option value="en">English</option>
+                        <option value="fr">French</option>
+                        <option value="ja">Japanese</option>
+                        <option value="pt">Portuguese</option>
+                        <option value="es">Spanish</option>
+                        <option value="zh">Chinese</option>
+                    </select>
+                </label>
+                <button type="submit" className="button-style">
+                    Translate
+                </button>
+            </form>
+            <label htmlFor="output">Translation:</label>
+            <output id="output" ref={outputRef}></output>
+        </>
+    );
 }
 
-export default App
+export default App;
